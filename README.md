b7ec59a712382d28bb799f55e99e1ee5cd0aa814
Danny McDonald <mcddjx@gmail.com>
2019-07-24 12:45:14 +0200

    black as night; add docs

--- README.md
+++ README.md
@@ -9,16 +9,41 @@ Validator for [marugoto](https://github.com/uzh/marugoto) content
 [![PyPI version](https://badge.fury.io/py/maruval.svg)](https://badge.fury.io/py/maruval)
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/python/black)
 
-## Install maruval
+## Installing dependencies
+
+*maruval* is a Python tool, so your machine needs Python and the Python package manager *pip* for installation. You should already have Python.
+
+To check if you have *pip*, type `which pip3` and/or `which pip` into your command line. If either command returns a path to `pip`, you can proceed to `Installing maruval`.
+
+To install *pip*, run the following command:
 
 ```bash
-pip install maruval
-# or
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && rm get-pip.py
+```
+
+Now `which pip` should show a path to a *pip* executable
+
+## Installing *maruval*
+
+Once you have *Python* and *pip3*/*pip*, run the following:
+
+```bash
+pip3 install maruval
+# or, from the git repo:
 git clone https://github.com/uzh/maruval && cd maruval && python.setup.py install
 ```
 
-## Using maruval:
+## How to use maruval (showing optional flags):
 
 ```bash
-maruval .
+maruval -f -nw <path_to_content>
+```
+
+* `-f`/`--fail-first` will stop *maruval* after first error
+* `-nw`/`--no-warnings` will suppress warning messages
+
+## Show help
+
+```bash
+maruval --help
 ```
--- maruval/maruval.py
+++ maruval/maruval.py
@@ -20,12 +20,12 @@ def _parse_cmdline_args():
     parser = argparse.ArgumentParser(description="Validate marugoto game content.")
 
     parser.add_argument(
-        "-w",
-        "--warnings",
+        "-nw",
+        "--no-warnings",
         default=True,
         action="store_true",
         required=False,
-        help="Display warnings",
+        help="Do not show warnings",
     )
 
     parser.add_argument(
@@ -50,7 +50,7 @@ def _get_json_files(path):
     out = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            if file.endswith('.json'):
+            if file.endswith(".json"):
                 out.append(os.path.join(root, file))
     return out
 
@@ -61,10 +61,10 @@ def _get_schemata():
     """
     schemata = dict()
     # hopfully, find the schemata folder?
-    data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'schemata')
-    jsons = [i for i in os.listdir(data_dir) if i.endswith('.json')]
+    data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "schemata")
+    jsons = [i for i in os.listdir(data_dir) if i.endswith(".json")]
     for jso in jsons:
-        with open(os.path.join(data_dir, jso), 'r') as fo:
+        with open(os.path.join(data_dir, jso), "r") as fo:
             loaded = json.load(fo)
             schemata[os.path.splitext(jso)[0]] = loaded
     return schemata
@@ -75,7 +75,7 @@ def _print_errors(errors):
     Nicely print out errors
     """
     for err, filename in errors:
-        form = 'Validation error for: {}\n\n{}'.format(filename, err)
+        form = "Validation error for: {}\n\n{}".format(filename, err)
         print(form)
 
 
@@ -84,7 +84,7 @@ def _get_correct_schema(json_file, schemata):
     Get correct validation schema based on filename
     """
     no_ext = os.path.basename(os.path.splitext(json_file)[0])
-    schema_name = ''.join([i for i in no_ext if i.isalpha()])
+    schema_name = "".join([i for i in no_ext if i.isalpha()])
     Draft7Validator.check_schema(schemata[schema_name])
     return schemata[schema_name]
 
@@ -105,7 +105,7 @@ def _locate_schemata():
     raise ValueError("No schemata found in: {}".format(dirs))
 
 
-def validate(path=None, fail_first=False, warnings=True):
+def validate(path=None, fail_first=False, no_warnings=False):
     """
     Main validator routine
 
@@ -118,21 +118,19 @@ def validate(path=None, fail_first=False, warnings=True):
     for json_file in to_check:
         schema = _get_correct_schema(json_file, schemata)
         try:
-            with open(json_file, 'r') as f:
+            with open(json_file, "r") as f:
                 data = json.load(f)
         except json.JSONDecodeError as err:
-            if fail_first:
-                _print_errors([(err, json_file)])
-                raise
             errors.append((err, json_file))
+            if fail_first:
+                break
             continue
         try:
             schema_validate(instance=data, schema=schema)
         except ValidationError as err:
-            if fail_first:
-                _print_errors([(err, json_file)])
-                raise
             errors.append((err, json_file))
+            if fail_first:
+                break
     _print_errors(errors)
 
 
